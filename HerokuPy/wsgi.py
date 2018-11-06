db2bc9a66762846ec59b105198a20efd5dddadf2
Tachibana <phuwit.nins@gmail.com>
2018-11-06 10:41:02 +0700

    modify.

--- HerokuPy/wsgi.py
+++ HerokuPy/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sample_app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HerokuPy.settings")
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
